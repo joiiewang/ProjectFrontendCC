8d55ec2230302216de3f996bac198ee817fba215
Ashley <ashleynicolebruce@gmail.com>
2020-10-23 17:25:05 +0000

    Connected all pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import CredsForm from './components/SaveCreds';
 import Navbar from './components/Navbar';
+import InfoPage from './components/InfoPage';
+import Links from "./components/Links";
+import Notes from "./components/Notes";
+import ToDoList from "./components/ToDoList";
+import Login from "./components/Login"
 
 function App() {
     return (
         <main>
             <Switch>
-		<Route path="/" component={Home} exact />
+		        <Route path="/" component={Home} exact />
                 <Route path="/creds" component={CredsForm} />
-	        <Route component={Error} />
+                <Route path="/InfoPage" component={InfoPage}/>
+                <Route path="/Links" component={Links}/>
+                <Route path="/Notes" component={Notes}/>
+                <Route path="/ToDoList" component={ToDoList}/>
+                <Route path="/Login" component={Login}/>
+	            <Route component={Error} />
             </Switch>
 	    <Navbar />
         </main>
--- src/components/Login.js
+++ src/components/Login.js
@@ -95,3 +95,5 @@ function Login(props) {
 		</div>
 	)
 }
+
+export default Login;
