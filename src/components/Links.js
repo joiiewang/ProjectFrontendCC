2ae5e0d7aae2b1d79110b28dd64f18c593fcc1b0
Zachary <zminshall@ucsb.edu>
2020-10-26 13:31:34 +0000

    Added a Links component to IndividualClass module

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {LinkList, SubmitForm} from './LinkList'
+import { LinkList, SubmitForm } from './LinkList'
 
 function Links() {
   return (
     <div>
-      <h1>Links Page</h1>
       <LinkList/>
-      <h1>Testing component</h1>
     </div>
   );
 };
