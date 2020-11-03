561ccbbade89835e5ac1520d296b90a1dbfc6e4f
Zachary <zminshall@ucsb.edu>
2020-11-03 08:39:02 +0000

    zm - added plant .svg files

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import './css/Plant.css'
+
+// EXAMPLE IMAGES (.SVG to resize well)
+import plant1 from './plantimages/plant1.svg'
+import plant2 from './plantimages/plant2.svg'
+import plant3 from './plantimages/plant3.svg'
+import plant4 from './plantimages/plant4.svg'
+import plant5 from './plantimages/plant5.svg'
+import plant6 from './plantimages/plant6.svg'
+
 
 class Plant extends React.Component {
 
@@ -14,15 +24,17 @@ class Plant extends React.Component {
     // Number of images and point values can be changed!
     choosePlant = (pointTotal) => {
         if(pointTotal < 5){
-            return "./plantimages/plant1.jpg"
+            return plant1
         } else if(pointTotal > 5 && pointTotal < 11) {
-            return "./plantimages/plant2.jpg"
+            return plant2
         } else if(pointTotal > 10 && pointTotal < 16) {
-            return "./plantimages/plant3.jpg"
+            return plant3
         } else if(pointTotal > 15 && pointTotal < 21) {
-            return "./plantimages/plant4.jpg"
+            return plant4
+        } else if(pointTotal > 20 && pointTotal < 26) {
+            return plant5
         } else {
-            return "./plantimages/plant5.jpg"
+            return plant6
         } 
     }
 
