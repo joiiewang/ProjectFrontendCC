c6b87876e714984416ca678c09c7d4dcb267f629
Ashley <ashleynicolebruce@gmail.com>
2020-11-04 01:25:34 +0000

    Pulling classes from backend - can't display yet

--- src/components/ShowClasses.js
+++ src/components/ShowClasses.js
@@ -3,12 +3,30 @@ import React from "react";
 class ShowClasses extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      classes : []
+    };
+
+  }
+  userName = "bob"
+  usernameString = ("https://project-backend-cc.herokuapp.com/api/v1/users/"+ this.userName +"/courses")
+
+  componentDidMount () {
+    fetch(this.usernameString).then(response => response.json()).then(data => this.setState({
+      classes: data
+    }))
+  }
+
+  mapClasses() {
+    console.log(this.state.classes.length + "We are in map classes")
+    return (this.state.classes.length)
   }
 
   render() {
+    console.log(this.state.classes)
     return (
       <div>
+        <p>{this.mapClasses()}</p>
         <button>
           <a href="/Course">Class 1</a>
         </button>
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,107 +1,86 @@
-import React from "react";
-import "./css/ToDoList.css";
+import React from 'react';
+import './css/ToDoList.css';
 class ToDoList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      toDoItems: [],
-    };
-  }
+  constructor () {
+        super()
+        this.state = {
+            toDoItems: []
+        }
+    }
 
   handleSubmit = (toDoItem) => {
-    this.setState({ toDoItems: [...this.state.toDoItems, toDoItem] });
-  };
-
+    this.setState({toDoItems: [...this.state.toDoItems, toDoItem]});
+  }
+  
   handleDelete = (index) => {
     const newToDoItemsArr = [...this.state.toDoItems];
     newToDoItemsArr.splice(index, 1);
-    this.setState({ toDoItems: newToDoItemsArr });
-  };
+    this.setState({toDoItems: newToDoItemsArr});
+  }
+  //Call api, get JSON with items and change state
 
   render() {
-    return (
-      <div>
-        <h1>To Do Page</h1>
-        <ToDoItemElements
-          toDoItems={this.state.toDoItems}
-          onDelete={this.handleDelete}
-        />
-        <SubmitForm onFormSubmit={this.handleSubmit} />
-      </div>
+    return(
+      
+        <div>
+          <h1>To Do Page</h1>
+          <ToDoItemElements toDoItems={this.state.toDoItems} onDelete= {this.handleDelete} />
+          <SubmitForm onFormSubmit={this.handleSubmit} />
+        </div>
+    
     );
-  }
+  } 
 }
 
 class SubmitForm extends React.Component {
-  state = { toDoItem: "", dueDate: "" };
+  state = { toDoItem: ''};
 
   handleSubmit = (e, r) => {
     e.preventDefault();
-    if (this.state.toDoItem === "") return;
-    if (this.state.dueDate === "") return;
-    this.props.onFormSubmit([this.state.toDoItem, this.state.dueDate]);
-    this.setState({ toDoItem: "" });
-    this.setState({ dueDate: "" });
-  };
+    if(this.state.toDoItem === '') return;
+    this.props.onFormSubmit([this.state.toDoItem]);
+    this.setState({ toDoItem: '' });
+    
+  }
   render() {
-    return (
+    return(
       <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="To Do Item"
+        <input 
+          type='text'
+          placeholder='Enter To Do Item'
           value={this.state.toDoItem}
-          onChange={(e) => this.setState({ toDoItem: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Due Date(yyyymmdd)"
-          value={this.state.dueDate}
-          onChange={(r) => this.setState({ dueDate: r.target.value })}
+          onChange={(e) => this.setState({toDoItem: e.target.value})}
         />
         <button>Add</button>
       </form>
     );
   }
+
 }
 
 const ToDoItemElements = (props) => {
   const todos = props.toDoItems.map((item, index) => {
-    return (
-      <Elem
-        toDoItem={item[0]}
-        dueDate={item[1]}
-        key={index}
-        id={index}
-        onDelete={props.onDelete}
-      />
-    );
-  });
-  console.log("2019-11-5" < "2018");
-  const sortedToDos = todos.sort(function (a, b) {
-    return a.props.dueDate - b.props.dueDate;
-  });
-  return <div className="toDoBox">{sortedToDos}</div>;
-};
-
-const Elem = (props) => {
-  const styles = {
-    fontSize: "5 px",
-  };
-  return (
-    <div className="element">
-      <input type="checkbox" />
-      <p>{props.toDoItem}</p>
-
-      <p style={styles}>{props.dueDate}</p>
-      <button
-        onClick={() => {
-          props.onDelete(props.id);
-        }}
-      >
-        Remove
-      </button>
+    return <Elem toDoItem={item[0]}  key={index} id={index} onDelete={props.onDelete} />
+  })
+  return( 
+    <div className = "toDoBox">
+      {todos}
     </div>
   );
-};
+}
+
+const Elem = (props) => {
+  return(
+    <div className = "element">
+      <input 
+          type="checkbox" 
+      />
+        <p>{props.toDoItem}</p>
+      <button onClick={() => {props.onDelete(props.id)}}>Remove</button>    
+
+    </div>
+  );
+}
+
 
 export default ToDoList;
