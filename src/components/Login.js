193935c813ba05dc543c9d47a04dc0cf762cf3c2
Ashley <ashleynicolebruce@gmail.com>
2020-11-17 20:41:26 +0000

    Login now only redirects you to home page when your credentials are correct

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Login(props) {
-  const [state, updateState] = useState({
-    userName: "",
-    password: "",
-    loggedIn: false,
-  });
+class Login extends React.Component {
+  constructor (props) {
+    super (props) 
+    this.state = {
+      userName: "",
+      password: "",
+      loggedIn: false,
+    }
 
-  function handleChange(evt) {
-    const name = evt.target.name;
-    const value = evt.target.value;
-    updateState({
-      ...state,
-      [name]: value,
-    });
+    this.handleChange = this.handleChange.bind(this);
+    this.saveCreds = this.saveCreds.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.set({ [name]: checked })
+      : this.setState({ [name]: value });
   }
 
 
-  const saveCreds = (evt) => {
+  saveCreds (evt) {
+
+    let currentComponent = this;
 
     evt.preventDefault();
-    sessionStorage.setItem('username', state.userName);
-    sessionStorage.setItem('password', state.password);
+    sessionStorage.setItem('username', this.state.userName);
+    sessionStorage.setItem('password', this.state.password);
+
 
   
     let server = "http://localhost:8118";
@@ -36,10 +43,32 @@ function Login(props) {
         server = "https://project-backend-cc.herokuapp.com";
       }
 
+      //const url = `${server}/api/v1/users/`;
+      const url = (`${server}/api/v1/users/${this.state.userName}/courses/`)
+      console.log(this.state.userName)
 
+      fetch(url, {
+        method: 'get',
+        headers: new Headers({
+          'Authorization': 'Basic '+btoa(this.state.userName+":"+this.state.password),
+          'Content-Type': 'application/json'
+        })
+      }).then(function(response){
+        if(!response.ok) {
+        throw new Error("HTTP status "+response.status)
+        }
+        else {
+          currentComponent.setState({loggedIn: true})
+          }
+        return response.json();
+      }).catch(error => alert(error));
 
+      const timer = setTimeout( () => {
+       console.log(currentComponent.state.loggedIn)
+       if (currentComponent.state.loggedIn) {
+        window.location.href = '/ShowClasses';
+       }}, 1000);
 
-    window.location.href = '/ShowClasses'; //Note: this reloads the page
     
     /*
     const url = `${server}/api/v1/users/`;
@@ -65,67 +94,68 @@ function Login(props) {
   };
 
 
+  render () {
+    const styles = {
+      margin: "auto",
+      width: "200px",
+      border: "3px solid green",
+      padding: "10px",
+      borderRadius: "25px",
+      backgroundColor: "#76FF5B",
+    };
+    const inputStyle = {
+      width: "90%",
+      margin: "auto",
+      borderRadius: "7px",
+      padding: "4px",
+      backgroundColor: "#CBFEC0",
+    };
+    const loginStyle = {
+      borderRadius: "7px",
+      padding: "4px",
+      marginLeft: "4%",
+      marginTop: "5px",
+      width: "90%",
+    };
+    const title = {
+      textAlign: "center",
+      fontSize: "25px",
+      fontWeight: "bold",
+    };
 
+    return (
+      <div>
+        <form style={styles}>
+          <p style={title}>Sign In</p>
+          <label>
+            Username:
+            <input
+              style={inputStyle}
+              type="text"
+              value={this.state.userName}
+              name="userName"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              style={inputStyle}
+              type="password"
+              value={this.state.password}
+              name="password"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button onClick={this.saveCreds}> Let's get planting </button>
+          <Link to="/NewUser">
+            <input type="submit" style={loginStyle} value="I'm a new seedling" />
+          </Link>
+        </form>
+      </div>
+    );
 
-  const styles = {
-    margin: "auto",
-    width: "200px",
-    border: "3px solid green",
-    padding: "10px",
-    borderRadius: "25px",
-    backgroundColor: "#76FF5B",
-  };
-  const inputStyle = {
-    width: "90%",
-    margin: "auto",
-    borderRadius: "7px",
-    padding: "4px",
-    backgroundColor: "#CBFEC0",
-  };
-  const loginStyle = {
-    borderRadius: "7px",
-    padding: "4px",
-    marginLeft: "4%",
-    marginTop: "5px",
-    width: "90%",
-  };
-  const title = {
-    textAlign: "center",
-    fontSize: "25px",
-    fontWeight: "bold",
-  };
-
-  return (
-    <div>
-      <form style={styles}>
-        <p style={title}>Sign In</p>
-        <label>
-          Username:
-          <input
-            style={inputStyle}
-            type="text"
-            value={state.userName}
-            name="userName"
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            style={inputStyle}
-            type="password"
-            value={state.seckey}
-            name="password"
-            onChange={handleChange}
-          />
-        </label>
-        <button onClick={saveCreds}> Let's get planting </button>
-        <Link to="/NewUser">
-          <input type="submit" style={loginStyle} value="I'm a new seedling" />
-        </Link>
-      </form>
-    </div>
-  );
+  }
 }
 
 export default Login;
