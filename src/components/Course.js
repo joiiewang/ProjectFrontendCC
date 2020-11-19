bdc14dc499f3a7b30191e65db474692262c024dd
Ashley <ashleynicolebruce@gmail.com>
2020-11-19 04:48:43 +0000

    Delete button on class page. Makes a popup window when you click on it

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NotesList, SubmitNoteForm } from "./NotesList";
 import { LinkList, SubmitLinkForm } from "./LinkList";
 import ToDoList from "./ToDoList";
+import Popup from "./Popup"
 
 class Course extends React.Component {
   constructor() {
@@ -11,29 +12,28 @@ class Course extends React.Component {
       toDoList: [],
       notes: [],
       links: [],
+      isOpen: false
     };
   }
 
-  /*
-  setUsername () {
-    this.setState({name : this.props.name})
-  }
-  
 
-  getCourseInfo () {
-    fetch ('https://project-backend-cc.herokuapp.com/api/v1/users/courses/' + this.state.name)
-  }
-  */
 
   render() {
-    //this.setUsername()
-    //this.getCourseInfo()
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        <LinkList/>
-        <NotesList/>
-        <ToDoList/>
+        <div>
+          <h1>{this.state.name}</h1>
+          <LinkList/>
+          <NotesList/>
+          <ToDoList/>
+        </div>
+
+        <button onClick= {(event) => this.setState({ isOpen: true})}>
+          Delete Class</button>
+
+        <Popup isOpen= {this.state.isOpen} onClose = {(event) => this.setState({isOpen: false})}>
+        </Popup>
+        
       </div>
     );
   }
@@ -45,4 +45,22 @@ export default Course;
         <LinkList links = {this.state.toDoList}/>
         <NotesList notes = {this.state.notes}/>
         <ToDoList todos = {this.state.toDoList}/>
+
+        <div className = "btn" onClick = {this.togglePop}>
+          <button>
+            Delete Class
+         </button>
+        </div>
+        {this.state.deleteClassSeen ? <Popup toggle = {this.togglePop}/> : null}
 */
+
+ /*
+  setUsername () {
+    this.setState({name : this.props.name})
+  }
+  
+
+  getCourseInfo () {
+    fetch ('https://project-backend-cc.herokuapp.com/api/v1/users/courses/' + this.state.name)
+  }
+  */
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,20 +1,52 @@
 import React from "react"
 
+
 export default class Popup extends React.Component {
-    handleClick = () => {
-        this.props.toggle();
-    };
-
+    
     render() {
+        let dialog = (
+            <div style = {dialogStyles}>
+                Are you sure you want to delete this class?
+                <button style = {dialogCloseButtonStyles} onClick= {this.props.onClose}>No</button>
+            </div>
+        )
+        if (! this.props.isOpen) {
+            dialog = null
+        }
         return (
-            <div className = "modal">
-                <div className = "modal_content">
-                    <span className = "close" onClick = {this.handleClick}>
-                        <p>Are you sure you wish to log out</p>
-                    </span>
-                </div>
-
+            <div>
+                {dialog}
             </div>
         )
     }
+}
+
+//{this.props.children}
+
+let dialogStyles = {
+    width: '500px',
+    maxWidth: '100%',
+    margin: '0 auto',
+    position: 'fixed',
+    left: '50%',
+    bottom: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '999',
+    backgroundColor: '#eee',
+    padding: '10px 20px 40px',
+    borderRadius: '8px',
+    display: 'flex',
+    flexDirection: 'column'
+}
+
+let dialogCloseButtonStyles = {
+    marginBottom: '15px',
+    padding: '3px 8px',
+    cursor: 'pointer',
+    borderRadius: '50%',
+    border: 'none',
+    width: '30px',
+    height: '30px',
+    fontweight: 'bold',
+    alignSelf: 'flex-end'
 }
